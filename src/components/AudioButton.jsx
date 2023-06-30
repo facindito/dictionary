93c6e22b0192eb9e34c9cfddaaf92cbb8a1ad3d5
facindito <facinditto@gmail.com>
2023-06-30 18:47:54 -0300

    add: icons components

--- src/components/AudioButton.jsx
+++ src/components/AudioButton.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from 'react'
+import { PauseIcon, PlayIcon } from './Icons'
 
 export function AudioButton ({ src }) {
   const [isPlaying, setIsPlaying] = useState(false)
@@ -16,17 +17,11 @@ export function AudioButton ({ src }) {
     <>
       <audio ref={audioRef} src={src} onEnded={handleAudioEnd} />
       <button onClick={handleClick} className='p-4 bg-sky-200 rounded-full'>
-        {isPlaying
-          ? (
-            <svg className='text-sky-500 w-6 h-6' xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor'>
-              <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 5.25v13.5m-7.5-13.5v13.5' />
-            </svg>
-            )
-          : (
-            <svg className='text-sky-500 w-6 h-6' xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor'>
-              <path strokeLinecap='round' strokeLinejoin='round' d='M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z' />
-            </svg>
-            )}
+        {
+          isPlaying
+            ? <PauseIcon className='text-sky-500 w-6 h-6' />
+            : <PlayIcon className='text-sky-500 w-6 h-6' />
+        }
       </button>
     </>
   )
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { SearchIcon } from './Icons'
 
 export function SearchForm ({ onSubmit }) {
   const [search, setSearch] = useState('')
@@ -16,7 +17,7 @@ export function SearchForm ({ onSubmit }) {
       <label htmlFor='default-search' className='mb-2 text-base font-medium text-gray-900 sr-only'>Search</label>
       <div className='relative'>
         <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
-          <svg aria-hidden='true' className='w-5 h-5 text-sky-500' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z' /></svg>
+          <SearchIcon />
         </div>
         <input
           type='search'
