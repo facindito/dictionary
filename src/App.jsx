4403569c0fac0d847df6b043f74aec77e7b53c38
facindito <facinditto@gmail.com>
2023-07-04 20:20:20 -0300

    feat: add home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router-dom'
 
 export function App () {
   return (
-    <>
+    <div className='flex flex-col h-screen max-w-2xl mx-auto'>
       <Header />
       <Outlet />
-    </>
+    </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,11 @@ import { SearchForm } from '../components/SearchForm'
 
 export default function Home () {
   return (
-    <SearchForm />
+    <main className='p-4 flex-1 flex flex-col justify-center items-center w-full'>
+      <div className='relative w-full'>
+        <SearchForm className='absolute top-0 w-full' />
+        <img src='/banner.svg' />
+      </div>
+    </main>
   )
 };
